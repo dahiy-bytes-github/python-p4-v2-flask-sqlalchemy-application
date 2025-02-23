eaf1e47ebecfdec1b2e24c26d88bfea704afb126
Abdullahi <aadahiye@gmail.com>
2025-02-23 12:02:35 +0300

    completes lab on Querying a Database in a Flask Application

--- server/app.py
+++ server/app.py
@@ -15,6 +15,30 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 # add views here 
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet directory!</h1>',
+        200
+    )
+    return response
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+    if pet is None:
+        return make_response('<p>pet not found.</p>', 404)
+    response  = f'<p>{pet.name} {pet.species}</p>'
+    return make_response(response, 200)
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species = species).all()
+    size  = len(pets)
+    response = f'<h2>Ther are {size} {species}</h2>'
+    
+    for pet in pets:
+        response += f'<p>{pet.name}</p>'
+    return make_response(response, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
